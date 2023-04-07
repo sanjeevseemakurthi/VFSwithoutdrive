bb38af86341fcde64198dbe08da8a6a505cfebd7
sanjeevseemakurthi <sanju.seemakurthi8@gmail.com>
2023-04-07 18:59:38 +0530

    auth cghanges

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
+declare var gapi: any
+declare global {
+  interface Window { handleCredentialResponse: any; }
+}
+window.handleCredentialResponse = (response: any) => {
 
+  console.log(response);
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +14,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'VFSwithoutdrive';
+  constructor() {
+  }
+  execute() {
+    return gapi.client.drive.files.list({})
+      .then(function (response: any) {
+        // Handle the results here (response.result has the parsed body).
+        console.log("Response", response);
+      },
+        function (err: any) { console.error("Execute error", err); });
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {MatTableModule} from '@angular/material/table';
+import { MatTableModule } from '@angular/material/table';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatSelectModule} from '@angular/material/select';
-import {MatTabsModule} from '@angular/material/tabs';
-import  AlertModule  from 'ngx-bootstrap';
-
+import { MatSelectModule } from '@angular/material/select';
+import { MatTabsModule } from '@angular/material/tabs';
+import AlertModule from 'ngx-bootstrap';
 
 @NgModule({
   declarations: [
